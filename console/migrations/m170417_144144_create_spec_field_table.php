68f42e52e56dbb63609ceaf47595c29af251a097
nadirvishun <nadirvishun@gmail.com>
2017-04-17 23:05:38 +0800

    增加区域价格表和规格字段表

--- console/migrations/m170417_144144_create_spec_field_table.php
+++ console/migrations/m170417_144144_create_spec_field_table.php
@@ -3,11 +3,11 @@
 use yii\db\Migration;
 
 /**
- * Handles the creation of table `region_price`.
+ * Handles the creation of table `spec_field`.
  */
-class m170417_135512_create_region_price_table extends Migration
+class m170417_144144_create_spec_field_table extends Migration
 {
-    const TBL_NAME = '{{%region_price}}';
+    const TBL_NAME = '{{%spec_field}}';
 
     /**
      * @inheritdoc
@@ -16,17 +16,15 @@ class m170417_135512_create_region_price_table extends Migration
     {
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB COMMENT="区域价格表"';
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB COMMENT="规格字段表"';
         }
 
         $this->createTable(self::TBL_NAME, [
-            'id' => $this->primaryKey()->comment('地区ID'),
-            'pid' => $this->integer()->notNull()->comment('地区父ID'),
-            'region_name' => $this->string(50)->notNull()->comment('地区名称'),
-            'transport_type' => $this->boolean()->unsigned()->notNull()->comment('运输方式，1直达，2中转'),
-            'depart_limitation' => $this->decimal(10, 2)->notNull()->comment('发车时效'),
-            'transport_limitation' => $this->decimal(10, 2)->notNull()->comment('运输时效'),
-            'pickup_limitation' => $this->decimal(10, 2)->notNull()->comment('提货时效'),
+            'id' => $this->primaryKey()->comment('规格ID'),
+            'field_name' => $this->string(50)->notNull()->comment('规格字段名称'),
+            'name' => $this->string(50)->notNull()->comment('规格名称'),
+            'min' => $this->decimal(10, 3)->notNull()->comment('最小值'),
+            'max' => $this->decimal(10, 3)->notNull()->comment('最大值'),
             'status' => $this->boolean()->unsigned()->notNull()->defaultValue(1)->comment('状态:0隐藏，1显示'),
             'sort' => $this->integer()->notNull()->defaultValue(0)->comment('排序'),
             'created_by' => $this->integer()->unsigned()->notNull()->comment('创建人'),
